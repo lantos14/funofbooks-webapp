4d10441948cdec9e07594620fb78b7813574d30e
lantost <lantos.tibor@kifu.gov.hu>
2019-02-15 13:35:15 +0100

    FEAT: add login page content

--- src/components/registration/LoginSheet.js
+++ src/components/registration/LoginSheet.js
@@ -6,7 +6,34 @@ export default class LoginSheet extends Component {
   render() {
     return (
       <div id="registration-sheet">
-        <h1>Login</h1>
+        <div className="field">
+          <p className="control has-icons-left has-icons-right">
+            <input className="input" type="email" placeholder="E-mail" />
+            <span className="icon is-small is-left">
+              <i className="fas fa-envelope"></i>
+            </span>
+            <span className="icon is-small is-right">
+              <i className="fas fa-check"></i>
+            </span>
+          </p>
+        </div>
+
+        <div className="field">
+          <p className="control has-icons-left">
+            <input className="input" type="password" placeholder="Jelszó" />
+            <span className="icon is-small is-left">
+              <i className="fas fa-lock"></i>
+            </span>
+          </p>
+        </div>
+
+        <div className="field">
+          <p className="control">
+            <button className="button is-success">
+              Bejelentkezem
+            </button>
+          </p>
+        </div>
       </div>
     );
   }
