ff45964324a84df68d4d303f04b4764c796fefa0
lantost <lantos.tibor@kifu.gov.hu>
2019-03-08 11:45:33 +0100

    REFACT: styling the fronted the right way

--- src/components/home/BookCard.js
+++ src/components/home/BookCard.js
@@ -11,41 +11,43 @@ export class BookCard extends Component {
     this.props.updateReviewedBook(e.currentTarget.id);
     history.push('/review');
 
-    scrollToTop(600);
+    scrollToTop(100);
   }
 
   render() {
     const { bookIndex, imgSrc, title, content, createdAt } = this.props;
+
     return (
-      <div id={bookIndex} className="column is-one-third card" onClick={this.handleOnClick}>
-        <div className="card-image">
-          <div className="book-image image is-4by3">
-            <img src={imgSrc} alt={`${title}-cover`}></img>
-          </div>
+      <div id={bookIndex} className='card' onClick={this.handleOnClick} >
+        <div className='card-image'>
+            <figure className='image'>
+              <img src={imgSrc}></img>
+            </figure>
         </div>
-        <div className="card-content">
-          <div className="media">
-            <div className="media-content">
-              <p className="title is-4">{title}</p>
+          <div className='card-content'>
+            <div className='media'>
+              <div className='media-content'>
+                <p className='title is-1'>{title}</p>
+              </div>
+            </div>
+            <div className='content'>
+              <p>{content}</p>
+              <p className='click-info'>Kattints a részletekért...</p>
+            </div>
+            <div className='card-date'>
+              <p><em>Feltöltve: {createdAt}</em></p>
             </div>
           </div>
-          <div className="content">
-            <p>{content}</p>
-          </div>
-          <div className='card-date'>
-            <p><em>Uploaded at: {createdAt}</em></p>
-          </div>
         </div>
-      </div>
-    );
-  }
-}
-
+        );
+      }
+    }
+    
 BookCard.propTypes = {
-  updateReviewedBook: PropTypes.func.isRequired,
-  bookIndex: PropTypes.number.isRequired,
-  imgSrc: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  content: PropTypes.string.isRequired,
-  createdAt: PropTypes.string.isRequired,
-};
+          updateReviewedBook: PropTypes.func.isRequired,
+        bookIndex: PropTypes.number.isRequired,
+        imgSrc: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        content: PropTypes.string.isRequired,
+        createdAt: PropTypes.string.isRequired,
+      };
